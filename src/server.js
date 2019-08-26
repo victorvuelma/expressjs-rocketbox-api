18180a25c460ef6159eec5638f96c2b72717abe2
Victor Vuelma <victorvuelma@outlook.com>
2019-08-26 17:53:02 -0300

    add mongodb connection

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect(
+  'mongodb+srv://rocketbox:rocketbox123@vuelma00-tz33q.mongodb.net/rocketbox?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true
+  })
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
