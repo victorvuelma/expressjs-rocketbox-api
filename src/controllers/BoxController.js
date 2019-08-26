0386dd8f2465cf464be460f3f65bdce1f8ea59ed
Victor Vuelma <victorvuelma@outlook.com>
2019-08-26 18:01:58 -0300

    create BoxController

---
+++ src/controllers/BoxController.js
@@ -0,0 +1,13 @@
+const Box = require('../models/Box')
+
+class BoxController {
+  async store (req, res) {
+    const box = await Box.create({
+      title: 'RocketBox'
+    })
+
+    return res.json(box)
+  }
+}
+
+module.exports = new BoxController()
