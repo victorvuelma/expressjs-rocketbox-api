7acfca0a2d271dcb00ae939205d5ae6025677d8d
Victor Vuelma <victorvuelma@outlook.com>
2019-08-26 18:07:32 -0300

    add first route

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ const express = require('express')
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  return res.send('Hello World')
-})
+const BoxController = require('./controllers/BoxController')
+
+routes.post('/boxes', BoxController.store)
 
 module.exports = routes
