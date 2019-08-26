8adc56f0c4c28fc588855cf9ffd5c894055e5a9c
Victor Vuelma <victorvuelma@outlook.com>
2019-08-26 18:28:10 -0300

    finish file upload to box

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,10 +1,20 @@
+const Box = require('../models/Box')
 const File = require('../models/File')
 
 class FileController {
   async store (req, res) {
-    console.log(req.file)
+    const box = await Box.findById(req.params.id)
 
-    return res.send('ok')
+    const file = await File.create({
+      title: req.file.originalname,
+      path: req.file.key
+    })
+
+    box.files.push(file)
+
+    await box.save()
+
+    return res.json(file)
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,11 @@ const BoxController = require('./controllers/BoxController')
 const FileController = require('./controllers/FileController')
 
 routes.post('/boxes', BoxController.store)
-routes.post('/files', multer(multerConfig).single('file'), FileController.store)
+routes.get('/boxes/:id', BoxController.show)
+routes.post(
+  '/boxes/:id/files',
+  multer(multerConfig).single('file'),
+  FileController.store
+)
 
 module.exports = routes
