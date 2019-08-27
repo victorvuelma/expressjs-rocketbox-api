f21980db9b16f1dba2da8e5f4617e1302f718495
Victor Vuelma <victorvuelma@outlook.com>
2019-08-27 14:28:04 -0300

    allow file download

--- src/models/File.js
+++ src/models/File.js
@@ -12,8 +12,18 @@ const File = new mongoose.Schema(
     }
   },
   {
-    timestamps: true
+    timestamps: true,
+    toObject: {
+      virtuals: true
+    },
+    toJSON: {
+      virtuals: true
+    }
   }
 )
+
+File.virtual('url').get(function () {
+  return `http://localhost:3000/files/${encodeURIComponent(this.path)}`
+})
 
 module.exports = mongoose.model('File', File)
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 
@@ -11,6 +12,7 @@ mongoose.connect(
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use(require('./routes'))
 
