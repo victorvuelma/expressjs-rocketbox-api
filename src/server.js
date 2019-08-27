036d609532d6ba373aaeb61e0e1e192b9c57758b
Victor Vuelma <victorvuelma@outlook.com>
2019-08-27 14:30:12 -0300

    setup socket.io

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const socket = require('socket.io')
 
 const app = express()
+const server = require('http').Server(app)
+const io = socket(server)
 
 mongoose.connect(
   'mongodb+srv://rocketbox:rocketbox123@vuelma00-tz33q.mongodb.net/rocketbox?retryWrites=true&w=majority',
   {
     useNewUrlParser: true
-  })
+  }
+)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -16,4 +20,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use(require('./routes'))
 
-app.listen(3000)
+server.listen(3000)
